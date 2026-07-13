80b1f21f3d595b51815865f2a4313caa14c6e924
CamiloAlarcon25 <camilo.alarcon.13@sansano.usm.cl>
2026-07-12 23:00:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function cambiarImagen(direccion) {
     
     imgElement.src = imagenesACV[indiceImagen];
 }
+// --- CARRUSEL PARKINSON ---
+const imagenesParkinson = ['park1.png', 'park2r.png', 'park3.png'];
+let indiceParkinson = 0;
+
+function cambiarImagenParkinson(direccion) {
+    const imgElement = document.getElementById('parkinson-image');
+    if (!imgElement) return;
+
+    indiceParkinson += direccion;
+    
+    if (indiceParkinson >= imagenesParkinson.length) indiceParkinson = 0;
+    if (indiceParkinson < 0) indiceParkinson = imagenesParkinson.length - 1;
+    
+    imgElement.src = imagenesParkinson[indiceParkinson];
+}
